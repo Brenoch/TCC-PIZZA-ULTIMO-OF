63f76885c7aab0eb5b45a59d90dcfec5998d1d37
Brenoch <103227487+Brenoch@users.noreply.github.com>
2023-11-17 13:27:56 -0300

    commit 17-11 breno viagem

---
+++ cd.php
@@ -0,0 +1,44 @@
+<?php
+
+include("conectcadastro.php");
+
+if (isset($_POST['registro'])) {
+    if(
+        strlen($_POST['nome_completo']) >= 1 &&
+        strlen($_POST['email']) >= 1 &&
+        strlen($_POST['senha']) >= 1 &&
+        strlen($_POST['confirmar_senha']) >= 1 &&
+        strlen($_POST['telefone']) >= 1 &&
+        strlen($_POST['cep']) >= 1 &&
+        strlen($_POST['endereco_entrega']) >= 1 &&
+        strlen($_POST['tipo_residencia']) >= 1
+       )
+     {
+            $name = trim($_POST['nome_completo']);
+            $email = trim($_POST['email']);
+            $senha = trim($_POST['senha']);
+            $confirmar_senha = trim($_POST['confirmar_senha']);
+            $telefone = trim($_POST['telefone']);
+            $cep = trim($_POST['cep']);
+            $endereco_entrega = trim($_POST['endereco_entrega']);
+            $tipo_residencia = trim($_POST['tipo_residencia']);
+            $fecha = date("d/m/y");
+            $consulta = "INSERT INTO datos(nome_completo, email, senha, confirmar_senha, telefone, cep, endereco_entrega, tipo_residencia, fecha) VALUES('$nome_completo', '$email', '$senha', '$confirmar_senha', '$telefone', '$cep', '$endereco_entrega', '$tipo_residencia', '$fecha')";
+            $resultado = mysqli_query($conex, $consulta);
+            if ($resultado) {
+             ?>
+                <h3 class="sucess" >Seu registro está completo</h3>
+             <?php
+            } else {
+             ?>
+                <h3 class="error">Ocorreu um erro</h3>
+             <?php
+            }
+        } else {
+            ?>
+                <h3 class="error">Preencha todos os campos</h3>
+            <?php
+        }
+}
+
+?>
---
+++ projeto-php.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 16/11/2023 às 23:58
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projeto-php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `profile_image` varchar(255) NOT NULL,
+  `bio` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `profile_image`, `bio`) VALUES
+(1, '1644199464-headfone.png', 'headfone pretokk');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `senha` varchar(20) NOT NULL,
+  `email` varchar(110) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `sexo` varchar(15) NOT NULL,
+  `data_nasc` date NOT NULL,
+  `cidade` varchar(45) NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  `endereco` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `senha`, `email`, `telefone`, `sexo`, `data_nasc`, `cidade`, `estado`, `endereco`) VALUES
+(1, 'pedro', '123', 'pedro@gmail.com', '21000000000', 'masculino', '2005-07-29', 'cg', 'rj', 'rua');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
